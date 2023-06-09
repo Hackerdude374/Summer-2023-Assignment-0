a5e7d74f466cbe286cfba34e1b5dd2e27dae918f
Hackerdude374 <136039952+Hackerdude374@users.noreply.github.com>
2023-06-09 17:27:10 -0400

    Update titleCaseEdit.js

--- 01-titleCaseEdit/titleCaseEdit.js
+++ 01-titleCaseEdit/titleCaseEdit.js
@@ -1,6 +1,18 @@
 function titleCaseEdit(title) {
   // Insert code here;
+  //first letter always cap
+  title[0].toUpperCase();
+  //find an individual word
+  const words = title.split(" "); 
+  //loop to find words, then capitalzie first letter and add the remaining letters.
+  for (let i = 0; i < words.length; i++) {
+    words[i] = words[i][0].toUpperCase() + words[i].substr(1);
 }
 
+const result = words.join(" ");
+
+return result; 
+}
+console.log(titleCaseEdit("the quick brown fox"));
 // Do not edit this line;
 module.exports = titleCaseEdit;
