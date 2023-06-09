3a26b20e4e5d306e898fe22b922bddf2639b4271
Hackerdude374 <136039952+Hackerdude374@users.noreply.github.com>
2023-06-09 17:54:45 -0400

    Update setUnionOfAnyAmountOfSets.js

--- 17-setUnionOfAnyAmountOfSets/setUnionOfAnyAmountOfSets.js
+++ 17-setUnionOfAnyAmountOfSets/setUnionOfAnyAmountOfSets.js
@@ -1,6 +1,16 @@
 function setUnionOfAnyAmountOfSets(...args) {
   // Insert code here;
+let result = new Set();
+  for(let set of args){ 
+    for(let num of set){ //add unique numbers in a set
+      result.add(num);
+    }
+  }
+  return result;
 }
+const s1 = new Set([1, 2, 4]);
+const s2 = new Set([4, 5, 6]);
+console.log(setUnionOfAnyAmountOfSets(s1,s2));
 
 // Do not edit this line;
 module.exports = setUnionOfAnyAmountOfSets;
