6cc760c8a5565a7f3b153f7006aa578172415ef5
Hackerdude374 <136039952+Hackerdude374@users.noreply.github.com>
2023-06-09 18:50:53 -0400

    Update sumOfAllEvenNumbers.js

--- 08-sumOfAllEvenNumbers/sumOfAllEvenNumbers.js
+++ 08-sumOfAllEvenNumbers/sumOfAllEvenNumbers.js
@@ -1,5 +1,15 @@
 function sumOfAllEvenNumbers(nums) {
   // Insert code here;
+  let evennumbers = [];
+  for(let i = 0; i < nums.length; i++){
+    if(nums[i] %2 == 0) //odd number
+      evennumbers.push(nums[i]);
+  }
+  let sum = 0;
+  for(let i = 0; i < evennumbers.length; i++){
+    sum = sum + evennumbers[i];
+  }
+  return sum;
 }
 
 // Do not edit this line;
