f470a96c7c4a19ea7a29e29c03add2a658160b25
Hackerdude374 <136039952+Hackerdude374@users.noreply.github.com>
2023-06-09 18:39:12 -0400

    Update sumOfNumsWithinARange.js

--- 10-sumOfNumsWithinARange/sumOfNumsWithinARange.js
+++ 10-sumOfNumsWithinARange/sumOfNumsWithinARange.js
@@ -1,5 +1,22 @@
 function sumOfNumsWithinARange(nums, start, end) {
   // Insert code here;
+  let sum = 0;
+  //find indexes 
+  let index1 = 0; //beginning
+  let index2 = 0; //end
+  for(let i = 0;i< nums.length; i++){
+    if(nums[i] == start){
+      index1 = i;
+    }
+    if(nums[i] == end){
+      index2 = i;
+    }
+  }
+  
+  for(let i = index1; i < index1; i++){
+    sum = nums[i] + sum;
+  }
+  return sum;
 }
 
 // Do not edit this line;
