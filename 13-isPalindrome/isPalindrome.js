d581f88bead7609cf80712da91f0391989157b57
Hackerdude374 <136039952+Hackerdude374@users.noreply.github.com>
2023-06-09 18:27:40 -0400

    Update isPalindrome.js

--- 13-isPalindrome/isPalindrome.js
+++ 13-isPalindrome/isPalindrome.js
@@ -1,6 +1,16 @@
 function isPalindrome(word) {
   // Insert code here;
+  //case 1 the first letter and last dont match = no palindrome 
+  if(word[0] != word[word.length -1]){
+    return false; 
+  }
+  if(word[0] == word[word.length -1]){
+    word === word.split('').reverse().join();
+    return word;
+  }
+  return false;
 }
-
+console.log(isPalindrome("big"));
+console.log(isPalindrome("mom"));
 // Do not edit this line;
 module.exports = isPalindrome;
