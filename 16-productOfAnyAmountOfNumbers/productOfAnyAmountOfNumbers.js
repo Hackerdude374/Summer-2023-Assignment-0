886379061d2281c2180d343d92f027bf67c205f1
Hackerdude374 <136039952+Hackerdude374@users.noreply.github.com>
2023-06-09 18:00:45 -0400

    Update productOfAnyAmountOfNumbers.js

--- 16-productOfAnyAmountOfNumbers/productOfAnyAmountOfNumbers.js
+++ 16-productOfAnyAmountOfNumbers/productOfAnyAmountOfNumbers.js
@@ -1,6 +1,11 @@
 function productOfAnyAmountOfNumbers(...args) {
   // Insert code here;
+  let result = 1; // any number multiplied by 1 is itself
+  for(let num of args){
+    result = num*result; //keep on multiplying
+  }
+  return result;
 }
-
+console.log(productOfAnyAmountOfNumbers(3, 4, 5));
 // Do not edit this line;
 module.exports = productOfAnyAmountOfNumbers;
