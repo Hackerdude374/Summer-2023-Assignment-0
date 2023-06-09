8991c6ea980bd5535a1edd63208b494a5a2b6e5b
Hackerdude374 <136039952+Hackerdude374@users.noreply.github.com>
2023-06-09 18:17:19 -0400

    Update frequencyCounter.js

--- 14-frequencyCounter/frequencyCounter.js
+++ 14-frequencyCounter/frequencyCounter.js
@@ -1,6 +1,16 @@
 function frequencyCounter(word) {
   // Insert code here;
+  let freq = {};
+  for(let i = 0; i < word.length; i++){
+    let char = word.charAt(i); //find letter
+    if(freq[char]){ //increment 1 every time found
+      freq[char]++;
+    }
+    else 
+      freq[char] = 1; //1 if no duplicates found
+  }
+  return freq;
 }
-
+console.log(frequencyCounter("computer"));
 // Do not edit this line;
 module.exports = frequencyCounter;
