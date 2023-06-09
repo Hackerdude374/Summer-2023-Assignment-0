0595116c7f326d3988a319995dc7c5abbeaf7d78
Hackerdude374 <136039952+Hackerdude374@users.noreply.github.com>
2023-06-09 18:07:27 -0400

    Update countDownSum.js

--- 15-countDownSum/countDownSum.js
+++ 15-countDownSum/countDownSum.js
@@ -1,9 +1,17 @@
 class MySolution {
   countDownSum(num) {
     // Insert code here;
+    let sum = num;// starting sum be the number.
+    let subtract = num-1;
+    for(let i = 0; i < num; i++){
+      sum = sum + subtract;
+      subtract--;
+    }
+    return sum;
   }
 }
 
 // Do not edit this line;
-let StudentSolution = MySolution;
+let StudentSolution = new MySolution();
+console.log(StudentSolution.countDownSum(5));
 module.exports = StudentSolution;
