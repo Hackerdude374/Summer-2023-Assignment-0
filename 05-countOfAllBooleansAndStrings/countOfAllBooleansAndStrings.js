57b03f4a44c20a189fc4dd68e5ae247e97f1b5e1
Hackerdude374 <136039952+Hackerdude374@users.noreply.github.com>
2023-06-09 18:57:21 -0400

    Update countOfAllBooleansAndStrings.js

--- 05-countOfAllBooleansAndStrings/countOfAllBooleansAndStrings.js
+++ 05-countOfAllBooleansAndStrings/countOfAllBooleansAndStrings.js
@@ -1,5 +1,11 @@
 function countOfAllBooleansAndStrings(arr) {
   // Insert code here;
+  let count = 0;
+  for(let i = 0; i < arr.length; i++){
+    if(arr[i] == "true" || arr[i] == true || arr[i] == "false" || arr[i] == "true")
+      count++;
+  }
+  return count;
 }
 
 // Do not edit this line;
