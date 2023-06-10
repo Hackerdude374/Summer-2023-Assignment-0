e9082d6ee208e64109a45a309502a8c8c5aab34e
Hackerdude374 <136039952+Hackerdude374@users.noreply.github.com>
2023-06-10 00:51:59 -0400

    Update binarySearch.js

--- 19-binarySearch/binarySearch.js
+++ 19-binarySearch/binarySearch.js
@@ -3,9 +3,22 @@ class MySolution {
     this.flag = false; // this is optional to use;
   }
 
-  binarySearch(nums, target) {
+  binarySearch(nums, target) { //nums is sorted. so faster search 
     // Insert code here;
+    let left = 0;
+    let right = nums.length -1;
+while(left <=right){
+    let middle = (left+right)/2;
+    if(nums[middle] == target){ //found
+      return true;
+    }
+    if(nums[middle] > target){ //less than, so decrement right.
+      right = middle -1;
+    }
+  if(nums[middle] < target){ //greater so increase left.
+    left = middle + 1;
   }
+  return false;
 }
 
 // Do not edit this line;
